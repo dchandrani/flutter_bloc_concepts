64ac7fafbc3df1a5988216472f1a0cb6c16265fa
Dhruv Chandrani <dhruv.chandrani@gmail.com>
2021-05-07 00:00:11 +0530

    Added support for hydrated bloc to counter cubit

--- lib/logic/cubit/counter_state.dart
+++ lib/logic/cubit/counter_state.dart
@@ -8,4 +8,23 @@ class CounterState {
     @required this.counter,
     this.wasIncremented,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'counter': counter,
+      'wasIncremented': wasIncremented,
+    };
+  }
+
+  factory CounterState.fromMap(Map<String, dynamic> map) {
+    return CounterState(
+      counter: map['counter'],
+      wasIncremented: map['wasIncremented'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CounterState.fromJson(String source) =>
+      CounterState.fromMap(json.decode(source));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ void main() async {
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
+  
   runApp(
     MyApp(
       appRouter: AppRouter(),
